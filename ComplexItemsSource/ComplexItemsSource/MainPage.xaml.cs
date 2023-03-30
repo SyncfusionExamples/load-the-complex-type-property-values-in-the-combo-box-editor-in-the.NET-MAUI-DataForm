679fe144d99945a356204192276ae9670b3473dc
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-03-30 11:30:55 +0530

    Added Sample.

---
+++ ComplexItemsSource/ComplexItemsSource/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ComplexItemsSource;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ComplexItemsSource/ComplexItemsSource/Sample/Behavior/DataFormBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.Maui.DataForm;
+
+namespace ComplexItemsSource
+{
+    public class DataFormBehavior : Behavior<SfDataForm>
+    {
+        protected override void OnAttachedTo(SfDataForm dataForm)
+        {
+            base.OnAttachedTo(dataForm);
+            if (dataForm != null )
+            {
+                dataForm.ItemsSourceProvider = new ItemSourceProvider();
+                dataForm.RegisterEditor("Gender", DataFormEditorType.RadioGroup);
+                dataForm.RegisterEditor("Address", DataFormEditorType.MultilineText);
+                dataForm.RegisterEditor("Department", DataFormEditorType.ComboBox);
+                dataForm.RegisterEditor("FeedBack", DataFormEditorType.MultilineText);
+                dataForm.GenerateDataFormItem += OnGenerateDataFormItem;
+            }
+        }
+        private void OnGenerateDataFormItem(object sender, GenerateDataFormItemEventArgs e)
+        {
+            if (e.DataFormItem!= null)
+            {
+                if (e.DataFormItem != null && e.DataFormItem.FieldName == "Department" && e.DataFormItem is DataFormComboBoxItem comboBoxItem)
+                {
+                    comboBoxItem.DisplayMemberPath = "Name";
+                    comboBoxItem.SelectedValuePath = "DepartmentID";
+                }
+            }
+        }
+        protected override void OnDetachingFrom(SfDataForm dataForm)
+        {
+            base.OnDetachingFrom(dataForm);
+           
+            if (dataForm != null)
+            {
+                dataForm.GenerateDataFormItem -= this.OnGenerateDataFormItem;
+            }
+        }
+    }
+}
